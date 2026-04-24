8dd98ae237968c9c5beac72832ea5efc57a875b6
LeeKiWoong <littlemanner1@gmail.com>
2026-04-24 22:57:53 +0900

    [Page - 149] UnassignedReference 오류! 널 상태에서 public일 때 발생!
    이 오류가 나왔다면 인스펙터 창에서 값을 넣어주자!

--- OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
@@ -2,11 +2,17 @@
 
 public class Compile_Error : MonoBehaviour
 {
+    //public 상태의 할당하지 않은 변수
+    public GameObject Target2;
+    
+    //public 상태가 아닌 할당하지 않은 변수
     GameObject Target;
     void Start()
     {
-        print("안녕하세요!");
+        //public 상태의 할당하지 않은 변수의 값 출력 : UnassignedReferenceException 에러
+        print(Target2.name);
 
+        //public 상태가 아닌 할당하지 않은 변수의 값 출력 : NullReferenceException 에러
         print(Target.name);
     }
 }
