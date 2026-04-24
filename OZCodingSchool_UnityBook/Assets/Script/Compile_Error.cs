b9773a3ebc39023eb8753b48b66d0b5d2a58e04a
LeeKiWoong <littlemanner1@gmail.com>
2026-04-24 22:32:29 +0900

    [Page - 148] NullReference오류! 널 체크를 꼭 하자!!
    지금까지도 앞으로도 쭈욱 중요한 널레퍼런스 오류! 널이 나올 수 있는 상황에선 항상 널 체크를 하는 습관을 들이자!!

--- OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
@@ -2,8 +2,11 @@
 
 public class Compile_Error : MonoBehaviour
 {
+    GameObject Target;
     void Start()
     {
-        print("안녕하세요!")
+        print("안녕하세요!");
+
+        print(Target.name);
     }
 }
