3aa6945b8d5c9dc2f3b7479f284abedd43db55eb
LeeKiWoong <littlemanner1@gmail.com>
2026-04-24 19:33:54 +0900

    [Page - 136] Find()를 사용해서 Main Camera 게임오브젝트 가져오기
    Find()를 사용해보았다.

--- OZCodingSchool_UnityBook/Assets/Script/First.cs
+++ OZCodingSchool_UnityBook/Assets/Script/First.cs
@@ -14,6 +14,8 @@ public class First : MonoBehaviour
     //Directional Light의 Light 컴포넌트를 담을 변수
     public Light DLlight;
 
+    //Main Camera를 검색해서 담을 변수1
+    public GameObject findCamera1;
 
     void Start()
     {
@@ -21,9 +23,13 @@ public class First : MonoBehaviour
         myTr = GetComponent<Transform>();
         myTr = transform;
 
+        //자신의 GameObject 컴포넌트를 가져와 변수에 할당
         myG = gameObject;
 
         //Directional Light 컴포넌트를 가져와 변수에 할당
         DLlight = tr.GetComponent<Light>();
+
+        //Main Camera를 이름으로 검색해 변수에 할당
+        findCamera1 = GameObject.Find("Main Camera");
     }
 }
