1fb90dea00d42a34e1440c913d80d733126e3893
LeeKiWoong <littlemanner1@gmail.com>
2026-04-24 18:38:29 +0900

    [Page - 132] GetComponent<T>()를 사용하여 원하는 특정 오브젝트의 컴포넌트 가져오기
    컴포넌트를 가져오자!

--- OZCodingSchool_UnityBook/Assets/Script/First.cs
+++ OZCodingSchool_UnityBook/Assets/Script/First.cs
@@ -5,8 +5,12 @@ public class First : MonoBehaviour
     //인스펙터 창에서 관리할 수 있는 Transform 자료형의 변수
     public Transform tr;
 
+    //자신의 Transform 컴포넌트를 담을 변수
+    public Transform myTr;
+
     void Start()
     {
-        
+        //자신의 Transform 컴포넌트를 가져와 변수에 할당
+        myTr = GetComponent<Transform>();
     }
 }
