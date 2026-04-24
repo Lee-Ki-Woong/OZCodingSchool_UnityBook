f481ce40277e10d5992e6cd77e91e844bb70a9c6
LeeKiWoong <littlemanner1@gmail.com>
2026-04-24 23:12:04 +0900

    [Page - 150]OutOfRange 크기가 있는 변수의 접근 가능한 범위를 벗어났을 때 발생하는 에러!
    이것도 은근 자주 발생할지도... 조심하자!!

--- OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Compile_Error.cs
@@ -9,6 +9,10 @@ public class Compile_Error : MonoBehaviour
     GameObject Target;
     void Start()
     {
+        //배열의 범위를 벗어난 인덱스 사용 : IndexOutOfRangeException 에러
+        int[] array = { 1, 2, 3 };
+        print(array[10]);
+
         //public 상태의 할당하지 않은 변수의 값 출력 : UnassignedReferenceException 에러
         print(Target2.name);
 
