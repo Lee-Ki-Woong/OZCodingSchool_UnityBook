e4644c89b11650c1c07c7a22881b7e2b3af24e37
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 16:19:36 +0900

    [Page 193~200] 씬 전환 및 씬 등록해보기
    책의 내용이랑 Unity6와 차이가 많이 나서 좀 힘들었다...

---
+++ OZCodingSchool_UnityBook/Assets/Script/StartSceneManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement; // SceneManager라는 유니티 도구를 사용하기 위함
+
+public class StartSceneManager : MonoBehaviour
+{
+    //Start 버튼을 클릭하면 호출
+    public void OnClickStart()
+    {
+        //GameScene 라는 이름의 씬 불러오기 (씬 전환)
+        SceneManager.LoadScene("GameScene");
+    }
+}
