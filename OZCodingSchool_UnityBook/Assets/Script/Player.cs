577c08b518d39ae817133b68c127da7d2c7a794d
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 18:50:49 +0900

    [Page 213] 마우스 입력 신호를 받아 "안녕"출력하기
    안녕!! Input.GetMouseButtonDown을 가지고 만들었기에 Project Settings에서 액티브 인풋 핸들링을 both로 설정하고 하였다.

---
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Update()
+    {
+        //마우스 좌클릭을 누르는 순간
+        if (Input.GetMouseButtonDown(0))
+        {
+            // "안녕" 이라는 문자열 출력
+            print("안녕");
+        }
+    }
+}
