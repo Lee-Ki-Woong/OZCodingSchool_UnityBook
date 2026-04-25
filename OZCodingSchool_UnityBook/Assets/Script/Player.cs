6678c74e1b2922eae3f0c17ec857e66db778266d
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 19:14:44 +0900

    [Page - 217] 인풋 매니저에서 누른 키를 float 자료형으로 가져와보기
    Vertical에 있는 키를 입력하니까 -1부터 1까지 숫자가 오르락 내리락한다!

--- OZCodingSchool_UnityBook/Assets/Script/Player.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -4,11 +4,7 @@ public class Player : MonoBehaviour
 {
     void Update()
     {
-        //위아래 방향키 또는 W키와 S키 중 하나라도 누르고 있다면
-        if(Input.GetButton("Vertical"))
-        {
-            // 플레이어 캐릭터 앞으로 이동
-            print("아!");
-        }
+        float v = Input.GetAxis("Vertical");
+        print(v);
     }
 }
