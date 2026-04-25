691e208fc285642527dbd2c8dcf0efd37ee19369
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 19:12:24 +0900

    [Page - 216] 인풋매니저를 통해 Vertical에 있는 키를 입력했는지 확인하기
    아!

--- OZCodingSchool_UnityBook/Assets/Script/Player.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -4,16 +4,11 @@ public class Player : MonoBehaviour
 {
     void Update()
     {
-        //마우스 좌클릭을 누르는 순간
-        if (Input.GetMouseButtonDown(0))
+        //위아래 방향키 또는 W키와 S키 중 하나라도 누르고 있다면
+        if(Input.GetButton("Vertical"))
         {
-            // "안녕" 이라는 문자열 출력
-            print("안녕");
-        }
-
-        if(Input.GetKey(KeyCode.W))
-        {
-            //플레이어 캐릭터 앞으로 이동
+            // 플레이어 캐릭터 앞으로 이동
+            print("아!");
         }
     }
 }
