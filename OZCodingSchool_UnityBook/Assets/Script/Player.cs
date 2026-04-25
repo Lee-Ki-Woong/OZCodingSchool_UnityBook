3accf7cd6bb7a258affbc54b917ba0d7caf8a577
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 19:32:18 +0900

    [Page - 218] 캐릭터의 이동기능 추가하기
    속도가 매우 빠르다... ㅋㅋㅋㅋㅋ Time.deltatime을 이용하자..

--- OZCodingSchool_UnityBook/Assets/Script/Player.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -4,7 +4,10 @@ public class Player : MonoBehaviour
 {
     void Update()
     {
+        //방향키 또는 WASD 입력을 숫자로 받아서 저장
+        float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
-        print(v);
+
+        transform.position += new Vector3(h, 0, v);
     }
 }
