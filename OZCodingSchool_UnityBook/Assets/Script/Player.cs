d05776504612b2292d4f134106d890d5d8c8cbfc
LeeKiWoong <littlemanner1@gmail.com>
2026-04-25 20:50:30 +0900

    [Page - 220] float를 자료형으로 가지는 필드를 추가하고, 그걸로 이동속도를 조절하자
    느릿느릿해졌다!

--- OZCodingSchool_UnityBook/Assets/Script/Player.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -2,12 +2,15 @@
 
 public class Player : MonoBehaviour
 {
+    public float m_moveSpeed; //이동 속도
+
     void Update()
     {
         //방향키 또는 WASD 입력을 숫자로 받아서 저장
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
-        transform.position += new Vector3(h, 0, v);
+        // x축에는 h의 값을, z축에는 v의 값을 계속 더하기
+        transform.position += new Vector3(h, 0, v) * m_moveSpeed;
     }
 }
