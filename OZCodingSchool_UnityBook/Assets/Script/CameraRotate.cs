e99c8bb4e3686a4bbc7d8cbf64ad6219859fe7d3
LeeKiWoong <littlemanner1@gmail.com>
2026-04-26 16:38:38 +0900

    [Page - 249] 쿼터니언이 아닌 오일러 각을 사용해서 각도 표현하기!!
    이제 콘솔창의 값과 인스펙터창의 값이 똑같게 나온다!!
    다만 eulerAngles는 0º~360º의 값만 처리할 수 있기 때문에 인스펙터창에 360º를 초과하는 값이나 음의 정수 값이 나올 수도 있다!!

--- OZCodingSchool_UnityBook/Assets/Script/CameraRotate.cs
+++ OZCodingSchool_UnityBook/Assets/Script/CameraRotate.cs
@@ -13,6 +13,7 @@ public class CameraRotate : MonoBehaviour
         //마우스가 움직인 만큼 X축 회전
         transform.Rotate(-mouseMoveY * RotateSpeed * Time.deltaTime, 0, 0);
 
-        print(transform.rotation.x);
+        // x축의 각도 출력
+        print(transform.eulerAngles.x);
     }
 }
