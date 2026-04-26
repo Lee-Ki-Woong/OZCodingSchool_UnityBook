a9ffd9758b54e726d2acc6d34e8e56b487a300b8
LeeKiWoong <littlemanner1@gmail.com>
2026-04-26 22:14:26 +0900

    [Page - 261] 파일을 직접 메모리에 로드하는 Resources.Load() 메서드를 사용하여 총알 가져오기
    이 메서드를 활용하면 실수를 줄일 수 있고 편리하지만, 직접 드래그&드롭하여 할당하는 것보다 더 많은 연산이 발생하기에 사용을 지양하는 게 좋다!

--- OZCodingSchool_UnityBook/Assets/Script/PlayerFire.cs
+++ OZCodingSchool_UnityBook/Assets/Script/PlayerFire.cs
@@ -17,8 +17,8 @@ public class PlayerFire : MonoBehaviour
         // 마우스 좌클릭을 누르는 순간
         if(Input.GetMouseButton(0))
         {
-            // 게임 안에 총알 프리팹의 복사본 생성
-            Instantiate(BulletPref);
+            // 게임 안에 리소스 폴더에서 불러오기한 총알 프리팹의 복사본 생성
+            Instantiate(Resources.Load("Bullet"));
         }
     }
 }
