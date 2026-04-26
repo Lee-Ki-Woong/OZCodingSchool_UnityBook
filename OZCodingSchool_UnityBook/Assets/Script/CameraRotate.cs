41269c2acf9144e487b1b3dfec7d2568865d8c99
LeeKiWoong <littlemanner1@gmail.com>
2026-04-26 16:20:12 +0900

    [Page - 247] 메인 카메라에 CameraRotate 스크립트를 단 후, 스크립트를 추가하여 상하 회전 구현하기
    마우스의 Y축(위/아래)움직임을 감지해 움직인 만큼 x축을 회전시키는 스크립트를 만들었다!!
    
    캐릭터는 이제 좌/우 뿐만 아니라 위/아래도 볼 수 있다!! 다만, 지금은 360도 회전이 가능하기에 일반적인 사람의 시야각으로 줄이는 옵션이 붙어야 한다..

---
+++ OZCodingSchool_UnityBook/Assets/Script/CameraRotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float RotateSpeed; // 회전 속도
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스의 위아래 움직임 입력을 숫자로 받아서 저장
+        float mouseMoveY = Input.GetAxis("Mouse Y");
+
+        //마우스가 움직인 만큼 X축 회전
+        transform.Rotate(-mouseMoveY * RotateSpeed * Time.deltaTime, 0, 0);
+    }
+}
