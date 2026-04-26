19ff6c29f9660fe09d7a3fec617055e6ffd900d1
LeeKiWoong <littlemanner1@gmail.com>
2026-04-26 21:26:06 +0900

    [Page - 259]마우스 우클릭을 하면 총알이 생성되는 스크립트를 만들었다!!
    마우스 우클릭을 하면 (0, 0, 0)에서 총알이 우수수 쏟아진다!! ㄷㄷ; 1초에 144개는 만들어지는 거 같다...

---
+++ OZCodingSchool_UnityBook/Assets/Script/PlayerFire.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class PlayerFire : MonoBehaviour
+{
+    // 총알 프리팹을 담아둘 변수
+    public GameObject BulletPref;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 마우스 좌클릭을 누르는 순간
+        if(Input.GetMouseButton(0))
+        {
+            // 게임 안에 총알 프리팹의 복사본 생성
+            Instantiate(BulletPref);
+        }
+    }
+}
