74cec6060798c996a40e281b7974b17c6d2170f1
LeeKiWoong <littlemanner1@gmail.com>
2026-04-26 12:42:30 +0900

    [Page - 231] Rigidbody를 스크립트로 불러온 후 AddForce로 점프를 구현하였다
    하지만 지금은 점프의 횟수 제한이 없어 무한정으로 올라갈 수 있다..

--- OZCodingSchool_UnityBook/Assets/Script/Player.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Player.cs
@@ -2,27 +2,44 @@
 
 public class Player : MonoBehaviour
 {
-    public float m_moveSpeed; //이동 속도
+    public float MoveSpeed; // 이동 속도
+    public float JumpPower; // 점프하는 힘
+
+    Rigidbody rb; // 플레이어의 Rigidbody 컴포넌트
+
+    private void Start()
+    {
+        // 플레이어의 Rigidbody 컴포넌트를 가져와서 저장
+        rb = GetComponent<Rigidbody>();
+    }
+
 
     void Update()
     {
-        //방향키 또는 WASD 입력을 숫자로 받아서 저장
+        // 방향키 또는 WASD 입력을 숫자로 받아서 저장
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
         // x축에는 h의 값을, z축에는 v의 값을 넣은 변수 생성
         Vector3 dir = new Vector3(h, 0, v);
 
-        //모든 방향의 속도가 동일하도록 정규화
+        // 모든 방향의 속도가 동일하도록 정규화
         dir.Normalize();
 
-        //이동할 방향에 원하는 속도 곱하기
-        //transform.position += dir * m_moveSpeed;
+        // 이동할 방향에 원하는 속도 곱하기
+        // transform.position += dir * m_moveSpeed;
 
         // x축에는 h의 값을, z축에는 v의 값을 계속 더하기
-        //transform.position += new Vector3(h, 0, v) * m_moveSpeed;
+        // transform.position += new Vector3(h, 0, v) * m_moveSpeed;
 
         // 이동할 방향에 원하는 속도 곱하기 (모든 기기에서 동일한 속도)
-        transform.position += dir * m_moveSpeed * Time.deltaTime;
+        transform.position += dir * MoveSpeed * Time.deltaTime;
+
+        // <Space> 키를 누른 순간
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // 위로 순간적인 힘 발생
+            rb.AddForce(Vector3.up * JumpPower, ForceMode.Impulse);
+        }
     }
 }
