20ffdb7a1ac72c3004669746d4c7eb76c5a439ab
LeeKiWoong <littlemanner1@gmail.com>
2026-04-27 15:54:28 +0900

    [Page - 289] Monster 프리팹을 Enemy로 바꾸고, Enemy 스크립트를 추가하였다!!
    Enemy 스크립트를 가진 오브젝트는 hp를 100 가진다!!

---
+++ OZCodingSchool_UnityBook/Assets/Script/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float hp = 100.0f; // 적의 체력
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+
+    }
+    
+    void Damaged(float damage)
+    {
+        // 공격 받은 데미지만큼 체력 감소
+        hp -= damage;
+    }
+}
