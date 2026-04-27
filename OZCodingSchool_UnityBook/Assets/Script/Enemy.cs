f38524740950d7b65d45d47fc49e14ac4940c1a2
LeeKiWoong <littlemanner1@gmail.com>
2026-04-27 17:39:50 +0900

    [Page - 293] 적 캐릭터의 체력 시각화
    [UI(Canvas)] - [Slider]를 추가하고, 슬라이더 컴포넌트의 값을 조정한 후, 스크립트에서 Hp와 슬라이더 컴포넌트를 연결하여 시각화 하였다!!
    이제 적의 체력이 감소하면 슬라이더의 빨간색 게이지가 줄어든다!!

--- OZCodingSchool_UnityBook/Assets/Script/Enemy.cs
+++ OZCodingSchool_UnityBook/Assets/Script/Enemy.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI; // UI 관련 클래스를 사용하기 위함
 
 public class Enemy : MonoBehaviour
 {
+    public Slider HpBar; // 적의 체력바
     public float Hp = 100.0f; // 적의 체력
 
 
@@ -19,5 +21,8 @@ public class Enemy : MonoBehaviour
     {
         // 공격 받은 데미지만큼 체력 감소
         Hp -= damage;
+
+        // 감소한 체력을 체력바에 표시
+        HpBar.value = Hp;
     }
 }
