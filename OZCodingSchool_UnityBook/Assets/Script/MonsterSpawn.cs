2d52017cf9d7c164650774865757e9873eec18a4
LeeKiWoong <littlemanner1@gmail.com>
2026-04-27 10:13:33 +0900

    [Page - 274+] 과제 제출을 위해서 몬스터 스포너를 만들었다
    LeftControl을 누르면 몬스터가 소환된다!

---
+++ OZCodingSchool_UnityBook/Assets/Script/MonsterSpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class MonsterSpawn : MonoBehaviour
+{
+    [SerializeField] private Transform MonsterSpawnPoint;
+    [SerializeField] private GameObject MonsterPref;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            StartMonsterSpawn();
+        }
+    }
+
+    private void StartMonsterSpawn()
+    {
+        if (MonsterSpawnPoint != null && MonsterPref != null)
+        {
+            Instantiate(MonsterPref, MonsterSpawnPoint.position, Quaternion.identity);
+        }
+    }
+}
